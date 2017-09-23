a3536b4fe3fa3f3f051aa2be89a7b7163bd9943c
Pavitra122 <pavitrashadvani122@gmail.com>
2017-09-23 15:15:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 10, 0, Math.PI*4, false);
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.closePath();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
 
+setInterval(draw, 10);
