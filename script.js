ac72e027dd3d44b01b86bca5b8f864ffa74d7708
Pavitra122 <pavitrashadvani122@gmail.com>
2017-09-23 14:59:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.arc(240, 160, 10, 0, Math.PI*4, false);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
